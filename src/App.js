d6b0757431f9f16a89e7629aa8baec84c2a40178
이재호 <jhlee@ibleaders.co.kr>
2021-10-05 13:48:37 +0900

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 import loader from '@ibsheet/loader'
 import Home from './components/home'
@@ -13,27 +13,26 @@ loader.config({
   }]
 })
 
-class App extends Component {
-  loaderVersion = loader.version
-  render() {
-    return (
-      <div className="App">
-        <p>@ibsheet/loader v{ this.loaderVersion }</p>
-        <BrowserRouter className="main">
-          <div className="top-nav">
-            <ul>
-              <li><Link to="/">Home</Link></li>
-              <li><Link to="/page1">Page1</Link></li>
-              <li><Link to="/page2">Page2</Link></li>
-            </ul>
-          </div>
-          <Route exact path="/" component={Home}/>
-          <Route exact path="/page1" component={Page1}/>
-          <Route exact path="/page2" component={Page2}/>
-        </BrowserRouter>
-      </div>
-    );
-  }
+const App = () => {
+  const loaderVersion = loader.version;
+
+  return (
+    <div className="App">
+      <p>@ibsheet/loader v{ loaderVersion }</p>
+      <BrowserRouter className="main">
+        <div className="top-nav">
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/page1">Page1</Link></li>
+            <li><Link to="/page2">Page2</Link></li>
+          </ul>
+        </div>
+        <Route exact path="/" component={ Home }/>
+        <Route exact path="/page1" component={ Page1 }/>
+        <Route exact path="/page2" component={ Page2 }/>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App
