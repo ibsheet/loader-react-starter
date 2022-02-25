49333096a1bd428a7335b331ffe451f16b4e35b4
이재호 <jhlee@ibleaders.co.kr>
2022-02-25 19:03:29 +0900

    move code

--- app.js
+++ app.js
@@ -3,27 +3,8 @@ const cors = require('cors');
 const app = express();
 const PORT = 8000;
 
-const info = {};
-const info2 = {};
-
-// sleep 기능
-const sleep = (ms) => {
-	return new Promise(resolve => {
-		setTimeout(resolve, ms);
-	});
-}
-
-// 다른 도메인, 포트에 있는 리소스를 요청을 허용하도록 CORS 허용
-app.use(cors());
-
-// page1.js 예제 rest api 를 이용한 ibpage
-app.get('/api/data/:ibpage', async (req, res) => {
-	// param 응답 전송 (param 확인 가능)
-  console.log("page 값은 " + req.params.ibpage + " 입니다");
-  // Total
-	info.Total = 15000;
-  // data
-	info.data = [
+const info = {
+  data: [
     { sa_nm: '홍길동', sa_id: '9821450', sa_dept: '04',
         sa_position: 'B0', sa_enterdate: '19980305', sa_desc: '' },
     { sa_nm: '김한국', sa_id: '9510427', sa_dept: '01',
@@ -84,7 +65,26 @@ app.get('/api/data/:ibpage', async (req, res) => {
         sa_position: 'B0', sa_enterdate: '19980305', sa_desc: '' },
     { sa_nm: '김한국', sa_id: '9510427', sa_dept: '01',
       sa_position: 'A3', sa_enterdate: '19890317', sa_desc: '' }
-  ];
+  ]
+};
+const info2 = {};
+
+// sleep 기능
+const sleep = (ms) => {
+	return new Promise(resolve => {
+		setTimeout(resolve, ms);
+	});
+}
+
+// 다른 도메인, 포트에 있는 리소스를 요청을 허용하도록 CORS 허용
+app.use(cors());
+
+// page1.js 예제 rest api 를 이용한 ibpage
+app.get('/api/data/:ibpage', async (req, res) => {
+	// param 응답 전송 (param 확인 가능)
+  console.log("page 값은 " + req.params.ibpage + " 입니다");
+  // Total
+	info.Total = 15000;
 	// sleep 실행
 	await sleep(700);
 	// json 응답을 전송.
