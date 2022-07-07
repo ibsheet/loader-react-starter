0f9f8077e8b71112699f70ad227c94119a59427a
이재호 <jhlee@softin.co.kr>
2022-07-07 15:45:21 +0900

    update sheetid

--- src/components/page1.js
+++ src/components/page1.js
@@ -3,14 +3,13 @@ import loader from '@ibsheet/loader';
 import { SheetSampleData } from '../shared/ibsheet-data';
 
 const Page1 = () => {
-  const sheetId = 'sheet';
+  let sheetId = '';
   const sheetEl = 'sheetContainer';
 
   useEffect(() => {
     const { data, options } = SheetSampleData[0];
 
     loader.createSheet({
-      id: sheetId,
       el: sheetEl,
       options,
       data
--- src/components/page2.js
+++ src/components/page2.js
@@ -3,14 +3,13 @@ import loader from '@ibsheet/loader';
 import { SheetSampleData } from '../shared/ibsheet-data';
 
 const Page2 = () => {
-  const sheetId = 'sheet';
+  let sheetId = '';
   const sheetEl = 'sheetContainer';
 
   useEffect(() => {
     const { data, options } = SheetSampleData[1];
 
     loader.createSheet({
-      id: sheetId,
       el: sheetEl,
       options,
       data
